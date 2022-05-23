70ba460f793a6fbef7e6aa21e735694be17ee3a6
AarnaJha <87459258+AarnaJha@users.noreply.github.com>
2022-05-23 15:44:38 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,27 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhPqG2tAvvukwLuLPJ3hVXw56ZMLn86Rk",
+  authDomain: "p94letschat2.firebaseapp.com",
+  projectId: "p94letschat2",
+  storageBucket: "p94letschat2.appspot.com",
+  messagingSenderId: "229290559243",
+  appId: "1:229290559243:web:124ac95bb612b6e68f040e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function adduser(){
+    localStorage.setItem("user_name" , user_name);
+    
+    window.location="kwitter_room.html";
+
+    user_name= document.getElementById("user_name").value;
+firebase.database().ref("/").child(user_name).update({
+    purpose:"adding user"
+});
+  
+    
+    }
+
+    
+    
